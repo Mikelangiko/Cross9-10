bfc554a2984cf0d6ba686e84b30f9796015c9677
Mikelangiko <mishakryttnt404@gmail.com>
2025-05-09 23:25:58 +0300

    lab9lok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyDk27VXVOfCsBC0R7VFhM3Y3CTR1u5gMCU',
-    authDomain: 'lab9-22d29.firebaseapp.com',
-    databaseURL: 'https://lab9-22d29-default-rtdb.firebaseio.com',
-    projectId: 'lab9-22d29',
-    storageBucket: 'lab9-22d29.firebasestorage.app',
-    messagingSenderId: '559407078467',
-    appId: '1:559407078467:web:bc79789005d53773ad88f6',
-    measurementId: 'G-EC22QPVGX2',
-  },
 };
